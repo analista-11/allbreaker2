3c1d3a083325ef73c8dc13ef71beabdfffac82f2
analista-11 <analista11@grupo-cubo.com>
2024-06-27 13:31:56 -0500

    27/06/2024-15

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo.gif';
+import logo from '../assets/logo.png';
 import '../components/Nav.css';
 
 const Nav = () => {
