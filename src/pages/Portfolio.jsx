b0076b1e12368bf270c06a5dfa1e2b8afe49abb5
analista-11 <analista11@grupo-cubo.com>
2024-11-23 07:44:55 -0500

    24

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,19 +6,18 @@ import portafolio1 from '../assets/portafolio1.jpg';
 
 const images = [
   { src: portafolio1, alt: 'Image 1', text: 'AD ASTRA FUERZA AEROESPACIAL COLOMBIANA', link: '/path1' },
-<<<<<<< HEAD
+
   { src: portafolio1, alt: 'Image 2', text: 'AD ASTRA FUERZA AEROESPACIAL COLOMBIANA', link: '/path2' },
   { src: portafolio1, alt: 'Image 3', text: 'AD ASTRA FUERZA AEROESPACIAL COLOMBIANA', link: '/path3' },
   { src: portafolio1, alt: 'Image 4', text: 'AD ASTRA FUERZA AEROESPACIAL COLOMBIANA', link: '/path4' },
   { src: portafolio1, alt: 'Image 5', text: 'AD ASTRA FUERZA AEROESPACIAL COLOMBIANA', link: '/path5' },
   { src: portafolio1, alt: 'Image 6', text: 'AD ASTRA FUERZA AEROESPACIAL COLOMBIANA', link: '/path6' },
-=======
+
   { src: portafolio1, alt: 'Image 2', text: 'AD ASTRA FUERZA AEROESPACIAL COLOMBIANA', link: '/path1' },
   { src: portafolio1, alt: 'Image 3', text: 'AD ASTRA FUERZA AEROESPACIAL COLOMBIANA', link: '/path1' },
   { src: portafolio1, alt: 'Image 4', text: 'AD ASTRA FUERZA AEROESPACIAL COLOMBIANA', link: '/path1' },
   { src: portafolio1, alt: 'Image 5', text: 'AD ASTRA FUERZA AEROESPACIAL COLOMBIANA', link: '/path1' },
-  { src: portafolio1, alt: 'Image 6', text: 'AD ASTRA FUERZA AEROESPACIAL COLOMBIANA', link: '/path1' },
->>>>>>> 7ccce293c945d4a97585a78d1b88c79d47c744c7
+  { src: portafolio1, alt: 'Image 6', text: 'AD ASTRA FUERZA AEROESPACIAL COLOMBIANA', link: '/path1' },ccce293c945d4a97585a78d1b88c79d47c744c7
 ];
 
 const Portfolio = () => {
